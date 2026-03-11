e0c54157e01d6b13a49ab2daab9a543519305c13
yazho <student-00-6f935e00d443@qwiklabs.net>
2026-03-11 10:16:04 +0000

    ok

--- main.go
+++ main.go
@@ -35,10 +35,3 @@ func blueHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
-
-func redHandler(w http.ResponseWriter, r *http.Request) {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
-}
